14e67e9a438fc469b00b9dd0e9b7eb4a53ad360f
Peter Tountas <petertountas1@gmail.com>
2017-02-02 19:40:21 -0600

    fubar

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
-
-
-
-	INSERT INTO burgers
-	(burger_name,devoured,data)
-VALUES
-	('bacon chedder burger',FALSE, CURRENT_TIMESTAMP),
-	('turkey burger',FALSE, CURRENT_TIMESTAMP),
-	('cheese burger',FALSE, CURRENT_TIMESTAMP);
